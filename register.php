67449e52ea417f15286e814be23fed85acd27fc4
reven0ir <michail.yashchuck@gmail.com>
2025-04-21 23:39:59 +0300

    refactor registration form and add server side validation

--- register.php
+++ register.php
@@ -1,83 +1,69 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/includes/db.php';
+require_once __DIR__ . '/includes/functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = load(['name', 'email', 'password']);
+
+    $v = new \Valitron\Validator($data);
+    $v->rules([
+        'required' => ['name', 'email', 'password'],
+        'email' => ['email'],
+        'lengthMin' => [
+                ['password', 6],
+        ],
+        'lengthMax' => [
+                ['name', 50],
+                ['email', 50],
+        ],
+    ]);
+
+    if ($v->validate()) {
+        echo 'Success';
+    } else {
+        debug($v->errors());
+    }
+}
 
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Guest Book :: Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-</head>
-<body>
+<?php
+require_once __DIR__ . '/views/includes/header.tpl.php'; ?>
 
-<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="register.php">Sign Up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="auth.php">Sign In</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                       aria-expanded="false">
-                        Hello, User
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="#">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Oops...</strong> Seems like error occurred..
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>Oops...</strong> Seems like error occurred..
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <form method="post">
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="name" placeholder="Name" name="name" value="<?= old('name'); ?>">
+                        <label for="name">Name</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email" value="<?= old('email'); ?>">
+                        <label for="email">Email</label>
+                    </div>
+
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="password" placeholder="Password"
+                               name="password">
+                        <label for="password">Password</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
+                </form>
+
             </div>
-
-            <form action="">
-
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="name" placeholder="Name">
-                    <label for="name">Name</label>
-                </div>
-
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
-                    <label for="email">Email</label>
-                </div>
-
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="password" placeholder="Password">
-                    <label for="password">Password</label>
-                </div>
-
-                <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
-            </form>
-
         </div>
     </div>
-</div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+require_once __DIR__ . '/views/includes/footer.tpl.php'; ?>
