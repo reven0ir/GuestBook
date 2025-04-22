d92d4ca6be17fee7463fa358fd5f260dfa802d99
reven0ir <michail.yashchuck@gmail.com>
2025-04-22 14:19:01 +0300

    add message handling logic to index.php

--- index.php
+++ index.php
@@ -8,4 +8,20 @@ require_once __DIR__ . '/includes/db.php';
 require_once __DIR__ . '/includes/functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
+if (isset($_POST['send-message'])) {
+    $data = load(['message']);
+    $v = new \Valitron\Validator($data);
+    $v->rules([
+        'required' => ['message'],
+    ]);
+
+    if ($v->validate()) {
+        if (save_message($data)) {
+            redirect('index.php');
+        }
+    } else {
+        $_SESSION['errors'] = get_errors($v->errors());
+    }
+}
+
 require_once __DIR__ . '/views/index.tpl.php';
