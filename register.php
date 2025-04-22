04d1f815cef74b3ef20fd9fefae34183f7fb48d9
reven0ir <michail.yashchuck@gmail.com>
2025-04-22 10:45:19 +0300

    refactor PHP views to use dedicated template files

--- register.php
+++ register.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/includes/db.php';
 require_once __DIR__ . '/includes/functions.php';
 
+$title = 'Sign Up';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = load(['name', 'email', 'password']);
 
@@ -31,49 +33,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-?>
-
-<?php require_once __DIR__ . '/views/includes/header.tpl.php'; ?>
-
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-
-                <?php if (isset($_SESSION['errors'])): ?>
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Oops...</strong>
-                        <?php
-                        echo $_SESSION['errors'];
-                        unset($_SESSION['errors']);
-                        ?>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                <?php endif; ?>
-
-                <form method="post">
-
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="name" placeholder="Name" name="name" value="<?= old('name'); ?>">
-                        <label for="name">Name</label>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email" value="<?= old('email'); ?>">
-                        <label for="email">Email</label>
-                    </div>
-
-                    <div class="form-floating">
-                        <input type="password" class="form-control" id="password" placeholder="Password"
-                               name="password">
-                        <label for="password">Password</label>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
-                </form>
-
-            </div>
-        </div>
-    </div>
-
-<?php
-require_once __DIR__ . '/views/includes/footer.tpl.php'; ?>
+require_once __DIR__ . '/views/register.tpl.php';
