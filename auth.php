e1422185ea279000cf40d347160d14b9e94a6025
reven0ir <michail.yashchuck@gmail.com>
2025-04-22 11:28:06 +0300

    fix typo and implement authentication logic handling

--- auth.php
+++ auth.php
@@ -2,7 +2,30 @@
 
 session_start();
 
-$title = 'Sing In';
+$title = 'Sign In';
 
 require_once __DIR__ . '/includes/db.php';
+require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = load(['email', 'password']);
+
+    $v = new \Valitron\Validator($data);
+    $v->rules([
+        'required' => ['email', 'password'],
+        'email' => ['email'],
+    ]);
+
+    if ($v->validate()) {
+        if (login($data)) {
+            redirect('index.php');
+        } else {
+            redirect('auth.php');
+        }
+    } else {
+        $_SESSION['errors'] = get_errors($v->errors());
+    }
+}
+
 require_once __DIR__ . '/views/auth.tpl.php';
