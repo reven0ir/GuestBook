cbca73c481fc835f04d421841a6917a2ea1e087b
reven0ir <michail.yashchuck@gmail.com>
2025-04-22 09:56:13 +0300

    add session-based error and success message handling

--- auth.php
+++ auth.php
@@ -1,53 +1,68 @@
 <?php
 
+session_start();
+
 require_once __DIR__ . '/includes/db.php';
 
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Guest Book :: Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-</head>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Guest Book :: Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+              crossorigin="anonymous">
+    </head>
 <body>
 
 <?php require_once __DIR__ . '/views/includes/header.tpl.php'; ?>
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>Oops...</strong> Seems like error occurred..
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
 
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong>Congratulation!</strong> Great success!
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+                <?php if (isset($_SESSION['errors'])): ?>
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Oops...</strong>
+                        <?php
+                            echo $_SESSION['errors'];
+                            unset($_SESSION['errors']);
+                        ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php endif; ?>
 
-
-            <form action="">
-
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
-                    <label for="email">Email</label>
+                <?php if (isset($_SESSION['success'])): ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Congratulation!</strong>
+                    <?php
+                    echo $_SESSION['success'];
+                    unset($_SESSION['success']);
+                    ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
+                <?php endif; ?>
 
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="password" placeholder="Password">
-                    <label for="password">Password</label>
-                </div>
+                <form action="">
 
-                <button type="submit" class="btn btn-primary mt-3">Sign In</button>
-            </form>
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                        <label for="email">Email</label>
+                    </div>
 
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <label for="password">Password</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mt-3">Sign In</button>
+                </form>
+
+            </div>
         </div>
     </div>
-</div>
 
 <?php require_once __DIR__ . '/views/includes/footer.tpl.php'; ?>
