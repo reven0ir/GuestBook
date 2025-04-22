e3f2c560f856cd47257ab774374473470db608c9
reven0ir <michail.yashchuck@gmail.com>
2025-04-22 12:01:20 +0300

    remove duplicate HTML structure from index template

--- views/index.tpl.php
+++ views/index.tpl.php
@@ -1,14 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Guest Book :: Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-</head>
-<body>
-
 <?php require_once __DIR__ . '/includes/header.tpl.php'; ?>
 
 <div class="container mt-5">
