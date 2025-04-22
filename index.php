c1b8ba89054418b063871e432b2a4de046991416
reven0ir <michail.yashchuck@gmail.com>
2025-04-22 11:29:01 +0300

    add session initialization and adjust script loading order

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once __DIR__ . '/includes/db.php';
-require_once __DIR__ . '/views/index.tpl.php';
+session_start();
 
 $title = 'Home';
+
+require_once __DIR__ . '/includes/db.php';
+require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__ . '/views/index.tpl.php';
